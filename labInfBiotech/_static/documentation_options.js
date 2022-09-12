6eebd392b4beee7f04e10506cbd8d4149735a49d
lucamarchetti79 <89861956+lucamarchetti79@users.noreply.github.com>
2022-09-12 16:24:45 +0200

    Primo_update_2022

--- labInfBiotech/_static/documentation_options.js
+++ labInfBiotech/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'a.a. 2021/2022',
+    VERSION: 'a.a. 2022/2023',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
